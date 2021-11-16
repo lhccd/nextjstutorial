519d59c5878a8c8a298872c39e013bcb140d10b9
Lorenz <67464618+lhccd@users.noreply.github.com>
2021-11-16 16:23:06 +0100

    finished tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,50 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
-export default function Home() {
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+export default function Home({allPostsData}) {
   return (
       <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
+          {/* Keep the existing code here */ }
+          <Head>
+              <title>{siteTitle}</title>
+          </Head>
+          <section className={utilStyles.headingMd}>
           <p>Hey everyone, this is my first Next.js website and above you can see a photo of my gorgeous girlfriend, who i love with all my heart.</p>
           <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
           </p>
-        </section>
+          </section>
+          {/* Add this <section> tag below the existing <section> tag */}
+          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+              <h2 className={utilStyles.headingLg}>Blog</h2>
+              <ul className={utilStyles.list}>
+                  {allPostsData.map(({ id, date, title}) => (
+                      <li className={utilStyles.listItem} key={id}>
+                          <Link href={`/posts/${id}`}>
+                              <a>{title}</a>
+                          </Link>
+                          <br />
+                          <small className={utilStyles.lightText}>
+                              <Date dateString={date} />
+                          </small>
+                      </li>
+                  ))}
+              </ul>
+          </section>
       </Layout>
   )
 }
